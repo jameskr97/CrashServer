60172ac5df816a0bce0022a58434f53ae23c4fd1
James <jameskr97@gmail.com>
2021-08-07 09:39:00 -0400

    [web] Change default port

--- src/main.py
+++ src/main.py
@@ -11,4 +11,4 @@ logger.addHandler(log_handler)
 # Run CrashServer
 if __name__ == '__main__':
     app = webapp.create_app()
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8081, debug=True)
