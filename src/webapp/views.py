187c205d2973563327499f76bb67f24a9755c4b0
James <jameskr97@gmail.com>
2021-08-07 01:29:31 -0400

    [web] Add views for additional pages

--- src/webapp/views.py
+++ src/webapp/views.py
@@ -2,6 +2,22 @@ from flask import Blueprint, render_template
 
 views = Blueprint("views", __name__)
 
+
 @views.route('/')
 def home():
     return render_template("home.html")
+
+
+@views.route('/crash-reports')
+def crash():
+    return render_template("crash.html")
+
+
+@views.route('/symbols')
+def symbols():
+    return render_template("symbols.html")
+
+
+@views.route('/upload')
+def upload():
+    return render_template("upload.html")
