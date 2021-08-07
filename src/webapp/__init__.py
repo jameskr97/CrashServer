916c4294320f1f95d55e7b1aed1f28db497d9360
James <jameskr97@gmail.com>
2021-08-07 04:50:03 -0400

    [web] Use Pathlib to ensure proper directories are references

--- src/webapp/__init__.py
+++ src/webapp/__init__.py
@@ -1,11 +1,16 @@
 from flask import Flask
 from .views import views
+from pathlib import Path
 
 def create_app() -> Flask:
     """
     Create Flask all object with all paths, error handlers, extensions, etc. registered.
     :return: Flask app object
     """
-    app = Flask("CrashServer")
+    resources_root = Path("res")
+    templates = resources_root / "templates"
+    static = resources_root / "static"
+
+    app = Flask("CrashServer", static_folder=static, template_folder=templates)
     app.register_blueprint(views)
     return app
