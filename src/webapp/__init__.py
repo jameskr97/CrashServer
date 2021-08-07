360ee128a579f003aa5b07e4923d841ec6849e93
James <jameskr97@gmail.com>
2021-08-07 09:41:05 -0400

    [Flask] Update app config to use absolute dirs, and load toml config

--- src/webapp/__init__.py
+++ src/webapp/__init__.py
@@ -1,16 +1,21 @@
 from flask import Flask
 from .views import views
 from pathlib import Path
+import toml
 
 def create_app() -> Flask:
     """
     Create Flask all object with all paths, error handlers, extensions, etc. registered.
     :return: Flask app object
     """
-    resources_root = Path("res")
+    resources_root = Path("res").absolute()
     templates = resources_root / "templates"
     static = resources_root / "static"
 
-    app = Flask("CrashServer", static_folder=static, template_folder=templates)
+    app = Flask("CrashServer", static_folder=static.name, template_folder=templates.name)
+    app.config.from_file(resources_root / "Config.toml", load=toml.load)
     app.register_blueprint(views)
+
+    # Create config directories
+    Path(app.config["MINIDUMP_STORE"]).absolute().mkdir(exist_ok=True)
     return app
