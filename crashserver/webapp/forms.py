f72ca901f65297ec72576520f30d5d35c5012a25
James <jameskr97@gmail.com>
2021-08-21 02:29:41 -0400

    Move forms from module to package

--- crashserver/webapp/forms.py
+++ crashserver/webapp/forms.py
@@ -1,9 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField
+from wtforms import StringField, PasswordField, BooleanField, SelectField
 from wtforms.validators import DataRequired, Email, Length
 
 class LoginForm(FlaskForm):
     email = StringField('Email Address', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=8, max=256)])
     remember_me = BooleanField('Remember Me')
+
+class CreateAppForm(FlaskForm):
+    title = StringField('Project Name')
+    project_type = SelectField('Project Type', choices=[('simple', "Simple"), ('versioned', "Versioned")])
 
