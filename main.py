05ef277eae45c43ac5d13e3ac84a56b4f4ee7777
James <jameskr97@gmail.com>
2021-08-24 00:49:01 -0400

    Format with Black

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ logger.addHandler(log_handler)
 from crashserver.config import settings
 from crashserver.webapp import app
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Set working directory to the directory of this file (project root)
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
     app.run(host="0.0.0.0", port=settings.flask.web_port, debug=True)
