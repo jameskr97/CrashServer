c10b5d206e8555ee174a4c268a353dbf6456e0ab
James <jameskr97@gmail.com>
2021-09-02 03:46:13 -0400

    Add domain to config

--- crashserver/webapp/__init__.py
+++ crashserver/webapp/__init__.py
@@ -29,6 +29,7 @@ def init_app() -> Flask:
     # Create app and inital parameters
     app = Flask("CrashServer", static_folder=str(static), template_folder=str(templates))
     app.config["SECRET_KEY"] = settings.flask.secret_key
+    app.config["SERVER_NAME"] = settings.flask.domain
 
     # Configure jinja2
     app.add_template_global(HumanBytes, "HumanBytes")
