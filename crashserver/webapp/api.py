6cfbfb846fe1ca48abfc11c85ec1bfcac3f0fce9
James <jameskr97@gmail.com>
2021-09-04 01:15:43 -0400

    Use LooseVersion instead of StrictVersion
    StrictVersion does not work with version numbers I predict I will need

--- crashserver/webapp/api.py
+++ crashserver/webapp/api.py
@@ -1,4 +1,4 @@
-from distutils.version import StrictVersion
+from distutils.version import LooseVersion
 import charset_normalizer as char_norm
 import itertools
 import operator
@@ -79,7 +79,7 @@ def get_symbols(project_id):
         # Creates callable that returns 'app_version' from object when you get_attr(object)
         # sym_dict is  in the format of {app_version: [Symbol objects of that version]}
         get_attr = operator.attrgetter("app_version")
-        sorted_list = sorted(proj_symbols, key=lambda x: StrictVersion(str(get_attr(x))), reverse=True)
+        sorted_list = sorted(proj_symbols, key=lambda x: LooseVersion(str(get_attr(x))), reverse=True)
         sym_dict = {
             version: sorted(list(group), key=operator.attrgetter("os"))
             for version, group in itertools.groupby(sorted_list, get_attr)
