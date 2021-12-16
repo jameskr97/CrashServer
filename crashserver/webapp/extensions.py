c31d48afea254d8d9fee39939d552568e713cf71
James <jameskr97@gmail.com>
2021-12-16 01:19:14 -0500

    Add s3 config and extension init

--- crashserver/webapp/extensions.py
+++ crashserver/webapp/extensions.py
@@ -1,3 +1,4 @@
+import boto3
 import redis
 import rq
 from flask_debugtoolbar import DebugToolbarExtension
@@ -7,11 +8,12 @@ from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
-from crashserver.config import get_redis_url
+from crashserver.config import get_redis_url, get_s3store_config
 
 debug_toolbar = DebugToolbarExtension()
 limiter = Limiter(key_func=get_remote_address)
 login = LoginManager()
 migrate = Migrate()
 db = SQLAlchemy()
+s3store = boto3.client("s3", **get_s3store_config())
 queue = rq.Queue("crashserver", connection=redis.Redis.from_url(get_redis_url()))
