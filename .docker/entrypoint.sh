8c70e4e33d9d4f87701efa39b16dfd1d5855ba81
James <jameskr97@gmail.com>
2022-01-02 06:29:31 -0500

    Update entrypoint user/groupadd to never fail
    Failures due to adding a group are because of the group already existing

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
 
-groupadd -r -g $PGID crashserver
-useradd  -r -u $PUID -g $PGID -m crashserver
+# || true to ignore if the groups already exist, and continue successfully.
+groupadd -r -g $PGID crashserver || true
+useradd  -r -u $PUID -g $PGID -m crashserver || true
 export HOME=/home/crashserver
 
 exec gosu crashserver:crashserver "$@"
